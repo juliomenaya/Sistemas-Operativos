d98eb68ca5e8a2b04bba077b8170e5dd2f462d64
juliomenaya <juliomenaya@gmail.com>
2016-04-26 03:24:04 -0500

    Version entregada.

--- principal.c
+++ principal.c
@@ -1,60 +1,64 @@
+/*Elaborado por Julio Méndez Ayala
+  Materia: Sistemas Operativos
+  PROGRAMA 1; Asignacion de memoria
+  Fecha de entrega: 26 de Abril del 2016
+  Version del programa: 1.0
+  Facultad de Ingenieria, UNAM*/
+
 #include<stdio.h>
 #include<stdlib.h>
 
-int disponible=3000;
-int memoria[4500];
-typedef struct p{
+int disponible=3000; 				//Variable para llevar conteo de memoria disponible
+int memoria[4500];					//Arreglo que simula las localidades de memoria
+
+typedef struct p{					//Estructura para guardar los datos de los procesos
 	int id;
 	int tama;
 	int inicio;
 }Proceso;
 
-/*typedef struct queue{
-	int id;
-	int inicio;
-	int tama;
-	struct queue *next;
-}Cola;
-*/
-
-typedef struct cola{
+typedef struct cola{				//Cola de procesos
 	Proceso encola;
 	int tam;
 	struct cola *atras;
 }Cola;
 
-int num_procs(FILE*);
-Proceso* guarda(int ,FILE*);
+int num_procs(FILE*);				//Prototipo de funciones
+Proceso* guarda(int ,FILE*);		
 void procesador(Proceso*,int);
 void asigna(Proceso*,int);
 int libera(Proceso);
 Cola* crea(Proceso);
 Cola* encolar(Cola* ,Proceso);
 
-
 int main(int argc, char* argv[]){
-	Proceso *proc;
-	int i;
-	FILE *archivo;
-	int procs;
-	if((archivo=fopen(argv[1],"r"))==NULL){
-		printf("\nNo se pudo abrir el archivo");
+	Proceso *proc;			//Apuntador al primer elementos del arreglo de procesos						
+	FILE *archivo;						
+	int procs;				//Variable para saber cuantos procesos tiene el archivo
+	if(argc<2){									
+		printf("\nOlvidaste indicar el nombre del archivo");
+		printf(" que contiene a los procesos\n");
+		printf("Saliendo del programa...\n");
+		return -1;
 	}
-	procs=num_procs(archivo);
+	if(argc>2){
+		printf("\nHay demasiados argumentos\n");
+		printf("Saliendo del programa...\n");
+		return -1;
+	}
+	if((archivo=fopen(argv[1],"r"))==NULL){				
+		printf("\nNo se pudo abrir el archivo / %s /",argv[1]);
+		printf("Saliendo del programa...\n");
+		return -1;
+	}
+	printf("\nEl archivo / %s / se abrio correctamente\n",argv[1]);
+	procs=num_procs(archivo);				//Lamadas a funciones 
 	proc=guarda(procs,archivo);
 	procesador(proc,procs);
-	//printf("id \ttama \t inicio\n ");
-	/*for(i=0;i<procs;i++){
-		printf("%d \t%d \t %d\n",proc[i].id,proc[i].tama,proc[i].inicio);
-	}*/
-	/*for(i=1500;i<1542;i++){
-		
-		printf("\nLocalidad[%d] = %d\n",i,memoria[i]);
-	}*/
 	fclose(archivo);
 	return 0;
 }
-
+//Funcion que calcula el numero de archivos que contenga el archivo
 int num_procs(FILE *archivo){
 	int n,tama=0;
 	while((n=fgetc(archivo))!=EOF){
@@ -64,7 +68,8 @@ int num_procs(FILE *archivo){
 	}
 	return tama+1;
 }
-
+//Funcion que guarda los datos del archivo en un arreglo dinamico de estructuras del
+// tipo Proceso
 Proceso* guarda(int proc,FILE *archivo){
 	Proceso *procs;
 	int i=0;
@@ -78,6 +83,8 @@ Proceso* guarda(int proc,FILE *archivo){
 	return procs;
 }
 
+//Funcion que determina si se debe asignar o liberar memoria a los procesos
+//Asi como tambien va creando la cola de procesos que no puedan ser asignados
 void procesador(Proceso *procs,int num_procs){
 	int i=0;
 	int k;
@@ -85,12 +92,10 @@ void procesador(Proceso *procs,int num_procs){
 	Cola *siguiente;
 	
 	for(i=0;i<num_procs;i++){
-		printf("\nputo2\n");
+		
 		if(procs[i].tama!=0){
-			printf("\nputo3\n");
+			
 			if(procs[i].tama<=disponible){
-				printf("\nputo4\n");
-				printf("\nid %d tama %d\n",procs[i].id,procs[i].tama);
 				asigna(&procs[i],i);
 				disponible-=procs[i].tama;
 				printf("\nSe asignaron %d localidades",procs[i].tama);
@@ -100,7 +105,6 @@ void procesador(Proceso *procs,int num_procs){
 				for(k=0;k<num_procs;k++){
 					printf("%d \t%d \t %d\n",procs[k].id,procs[k].tama,procs[k].inicio);
 				}
-			
 			}else{
 				printf("\nNo hay memoria suficiente");
 				printf("\nEl proceso %d entro en cola\n",procs[i].id);
@@ -109,11 +113,11 @@ void procesador(Proceso *procs,int num_procs){
 				for(k=0;k<num_procs;k++){
 					printf("%d \t%d \t %d\n",procs[k].id,procs[k].tama,procs[k].inicio);
 				}
-				/*if(inicio==NULL)
+				if(inicio==NULL)
 					inicio= crea(procs[i]);
 				else{
 					siguiente= encolar(inicio,procs[i]);
-				}*/
+				}
 			}
 		}else{
 			disponible+=libera(procs[i]);
@@ -121,7 +125,6 @@ void procesador(Proceso *procs,int num_procs){
 			printf("memoria restante %d\n",disponible);
 			//revisar procesos en cola y asignar
 		}	
-		printf("\nputo\n");
 	}
 	printf("\nSe atendio a todos los procesos\n");
 	printf("id \ttama \t inicio\n ");
@@ -131,33 +134,28 @@ void procesador(Proceso *procs,int num_procs){
 	printf("\nMemoria restante %d\n",disponible);
 	return;
 }
-
+//Funcion que asigna memoria a los procesos
 void asigna(Proceso *proc,int i){
 	static int k;
 	static int sig_ini;
-	printf("\nputo5\n");
+	
 	if(i==0){
-		printf("\nputo6\n");
+		
 		for(k=(*proc).inicio;k<proc->inicio+proc->tama;k++){
 			memoria[k]=proc->id;
 		}
 		sig_ini=k;
 	}else{
-		printf("\nputo7\n");
-		printf("\n sig antes %d\n",proc->inicio);
 		proc->inicio=sig_ini;
-		printf("\n sig desp %d\n",proc->inicio);
-		printf("\n tama %d\n",proc->tama);
-		for(k=proc->inicio;k<proc->inicio+proc->tama;k++){
-			//printf("\nputo8\n");
+		for(k=proc->inicio;k<proc->inicio+proc->tama;k++){	
 			memoria[k]=proc->id;
 		}	
 		sig_ini=k;
-
 	}
 	return;
 }
 
+//Funcion que quita de memoria al proceso que es pasado como parametro
 int libera(Proceso proc){
 	int i;
 	int cuanto_borro=0;
@@ -169,44 +167,28 @@ int libera(Proceso proc){
 			break;
 		}
 	}
-	
 	for(i=borra_desde;i<4500;i++){
 		if(memoria[i]==proc.id)
 			cuanto_borro++;
 	}
-	
 	for(i=borra_desde;i<borra_desde+cuanto_borro;i++){
 		memoria[i]=0;
 	}
 	return cuanto_borro;
 }
-/*
-void crea(Cola *primero,Cola *ultimo,Proceso *proc){
-	Cola nuevo;
-	nuevo=(Cola)malloc(sizeof(Cola));
-	nuevo->next=NULL;
-	
-	return;
-}
-*/
-
+//Funcion para crear el primer elemento de la cola de procesos
 Cola* crea(Proceso proc){
 	Cola *primero;
 	primero=(Cola*)malloc(sizeof(Cola));
 	primero->encola=proc;
 	primero->tam++; 
 	primero->atras= NULL;
-
-	printf("\nid %d tama %d\n",primero->encola.id,primero->encola.tama);
 	return primero;
 }
-
-
+//Funcion para agregar elementos a la cola de procesos
 Cola* encolar(Cola *siguiente,Proceso proc){
 	siguiente->atras= (Cola*)malloc(sizeof(Cola));
 	siguiente->encola= proc;
 	siguiente->tam++;
-	printf("\nid %d tama %d\n",siguiente->encola.id,siguiente->encola.tama);
-
 	return siguiente;
 }
